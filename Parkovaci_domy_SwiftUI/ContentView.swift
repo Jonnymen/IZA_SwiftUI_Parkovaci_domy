2d7698c853965209e5b84d0d6049f3523c0ae315
Jan Menšík <jonny.mensik@gmail.com>
2020-05-01 16:20:17 +0200

    Initial Commit

---
+++ Parkovaci_domy_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parkovaci_domy_SwiftUI
+//
+//  Created by Jan Menšík on 01/05/2020.
+//  Copyright © 2020 Jan Menšík. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
